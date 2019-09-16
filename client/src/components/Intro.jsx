e0026c145c2e4fb8e532b61c643ac842c42ea715
fjplaurr <fjplaurr@gmail.com>
2019-09-16 07:13:12 +0200

    Design change

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import personalLogo from '../../images/personalLogo.svg';
+import personalLogoF from '../../images/personalLogoF.png';
+
 
 
 
 export default function intro() {
     return (
         <div className="intro" id="intro">
-            <h1>Francisco Plaza</h1>
-            <h2>Web developer <span className="fas fa-rocket"></span></h2>
-            <p>Hi, I am an engineer with passion for programming. I try to make products that provide a good user experience following two principles: responsivness and simplicity.</p>
+            <div className="introDescription">
+                <h1>Hi, <br/>I am <span><img src={personalLogoF}></img></span>rancisco Plaza, <br/>web developer.</h1>
+                <p>Hi, I am an engineer with passion for programming. I try to make products that provide a good user experience following two principles: responsivness and simplicity.</p>
+            </div>
+            <img className="personalLogo" src={personalLogo}></img>
         </div>
     )
 }
--- client/src/components/ProjectsContainer.jsx
+++ client/src/components/ProjectsContainer.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import drumMachineGif from '../../images/drumMachine.gif';
-import drumMachine from '../../images/drumMachine1.png';
-
 
 
 export default function ProjectsContainer() {
@@ -28,35 +25,56 @@ export default function ProjectsContainer() {
                     </div>
                 </article>
                 <article>
-                    <img src="https://res.cloudinary.com/fjplaurr/image/upload/v1565777165/Web%20Development/Images/beach_woly2t.jpg" alt="" />
+                    <img className="drumMachineImg"></img>
                     <div className="projectInfo">
-                        <h3>Nisl placerat</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
+                        <h3>Drum Machine</h3>
+                        <p>App that allows the user to play a virtual drum machine using different sounds and voices.</p>
+                        <ul className="technologies">
+                            <li>React</li>
+                            <li>Javascript</li>
+                            <li>CSS Flexbox</li>
+                            <li>CSS Grid Layout</li>
+                            <li>HTML5</li>
+                        </ul>
                         <ul className="appRepoButtons">
-                            <li><a href="#" className="appAnchor">See app</a></li>
-                            <li><a href="#" className="repoAnchor">See repository</a></li>
+                            <li><a href="https://codepen.io/fjplaurr/full/GVQbQe" target="_blank" className="appAnchor">See app</a></li>
+                            <li><a href="https://github.com/fjplaurr/drumMachine" className="repoAnchor">See repository</a></li>
                         </ul>
                     </div>
                 </article>
                 <article>
-                    <img src="https://res.cloudinary.com/fjplaurr/image/upload/v1565777165/Web%20Development/Images/beach_woly2t.jpg" alt="" />
+                    <img className="drumMachineImg"></img>
                     <div className="projectInfo">
-                        <h3>Ante fermentum</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
+                        <h3>Drum Machine</h3>
+                        <p>App that allows the user to play a virtual drum machine using different sounds and voices.</p>
+                        <ul className="technologies">
+                            <li>React</li>
+                            <li>Javascript</li>
+                            <li>CSS Flexbox</li>
+                            <li>CSS Grid Layout</li>
+                            <li>HTML5</li>
+                        </ul>
                         <ul className="appRepoButtons">
-                            <li><a href="#" className="appAnchor">See app</a></li>
-                            <li><a href="#" className="repoAnchor">See repository</a></li>
+                            <li><a href="https://codepen.io/fjplaurr/full/GVQbQe" target="_blank" className="appAnchor">See app</a></li>
+                            <li><a href="https://github.com/fjplaurr/drumMachine" className="repoAnchor">See repository</a></li>
                         </ul>
                     </div>
                 </article>
                 <article>
-                    <img src="https://res.cloudinary.com/fjplaurr/image/upload/v1565777165/Web%20Development/Images/beach_woly2t.jpg" alt="" />
+                    <img className="drumMachineImg"></img>
                     <div className="projectInfo">
-                        <h3>Fusce consequat</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
+                        <h3>Drum Machine</h3>
+                        <p>App that allows the user to play a virtual drum machine using different sounds and voices.</p>
+                        <ul className="technologies">
+                            <li>React</li>
+                            <li>Javascript</li>
+                            <li>CSS Flexbox</li>
+                            <li>CSS Grid Layout</li>
+                            <li>HTML5</li>
+                        </ul>
                         <ul className="appRepoButtons">
-                            <li><a href="#" className="appAnchor">See app</a></li>
-                            <li><a href="#" className="repoAnchor">See repository</a></li>
+                            <li><a href="https://codepen.io/fjplaurr/full/GVQbQe" target="_blank" className="appAnchor">See app</a></li>
+                            <li><a href="https://github.com/fjplaurr/drumMachine" className="repoAnchor">See repository</a></li>
                         </ul>
                     </div>
                 </article>
