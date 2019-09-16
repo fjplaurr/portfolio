b7ed7774bd7948f54b161d7dc3f36354b23b8bad
fjplaurr <fjplaurr@gmail.com>
2019-09-16 20:25:37 +0200

    Design change

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -9,8 +9,10 @@ export default function intro() {
     return (
         <div className="intro" id="intro">
             <div className="introDescription">
-                <h1>Hi, <br/>I am <span><img src={personalLogoF}></img></span>rancisco Plaza, <br/>web developer.</h1>
-                <p>Hi, I am an engineer with passion for programming. I try to make products that provide a good user experience following two principles: responsivness and simplicity.</p>
+                <div className="introSentence">
+                    <h1 >Hello, <br />I am <span><img src={personalLogoF}></img>rancisco</span> Plaza, <br />web developer.</h1>
+                </div>
+                <p>Hello, I am an engineer with passion for programming. I try to make products that provide a good user experience following two principles: responsivness and simplicity.</p>
             </div>
             <img className="personalLogo" src={personalLogo}></img>
         </div>
