27b6a0f497e95cb6fcc9e44bb076f974f652e2e1
fjplaurr <fjplaurr@gmail.com>
2019-09-18 20:46:21 +0200

    Design change

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import personalLogo from '../../images/personalLogo.svg';
-import personalLogoF from '../../images/personalLogoF.png';
-
 
 
 
@@ -10,7 +8,8 @@ export default function intro() {
         <div className="intro" id="intro">
             <div className="introDescription">
                 <div className="introSentence">
-                    <h1 >Hello, <br />I am <span><img src={personalLogoF}></img>rancisco</span> Plaza, <br />web developer.</h1>
+                    {/*<h1 >Hello, <br />I am <span><img src={personalLogoF}></img>rancisco </span><span><img src={personalLogoP}></img>laza</span><br />, web developer.</h1>*/}
+                    <h1 >Hello, <br />I am <span className="name">F</span>rancisco<span className="surname"> P</span>laza<br />, web developer.</h1>
                 </div>
                 <p>Hello, I am an engineer with passion for programming. I try to make products that provide a good user experience following two principles: responsivness and simplicity.</p>
             </div>
--- client/src/components/ProjectsContainer.jsx
+++ client/src/components/ProjectsContainer.jsx
@@ -7,7 +7,7 @@ export default function ProjectsContainer() {
             <h2>Projects</h2>
             <div className="projects">
                 <article>
-                    <img className="drumMachineImg"></img>
+                    <div className="image drumMachineImg"></div >
                     <div className="projectInfo">
                         <h3>Drum Machine</h3>
                         <p>App that allows the user to play a virtual drum machine using different sounds and voices.</p>
@@ -25,43 +25,41 @@ export default function ProjectsContainer() {
                     </div>
                 </article>
                 <article>
-                    <img className="drumMachineImg"></img>
+                    <div className="image markdownPreviewer"></div >
                     <div className="projectInfo">
-                        <h3>Drum Machine</h3>
-                        <p>App that allows the user to play a virtual drum machine using different sounds and voices.</p>
+                        <h3>Markdown Previewer</h3>
+                        <p>App that allows the user to write Markdown language easily.</p>
                         <ul className="technologies">
                             <li>React</li>
                             <li>Javascript</li>
-                            <li>CSS Flexbox</li>
-                            <li>CSS Grid Layout</li>
+                            <li>CSS3</li>
                             <li>HTML5</li>
                         </ul>
                         <ul className="appRepoButtons">
-                            <li><a href="https://codepen.io/fjplaurr/full/GVQbQe" target="_blank" className="appAnchor">See app</a></li>
-                            <li><a href="https://github.com/fjplaurr/drumMachine" className="repoAnchor">See repository</a></li>
+                            <li><a href="https://codepen.io/fjplaurr/full/qzwxPy" target="_blank" className="appAnchor">See app</a></li>
+                            <li><a href="https://github.com/fjplaurr/reactMarkdownPreviewer" className="repoAnchor">See repository</a></li>
                         </ul>
                     </div>
                 </article>
                 <article>
-                    <img className="drumMachineImg"></img>
+                    <div className="image quoteMachineImg"></div >
                     <div className="projectInfo">
-                        <h3>Drum Machine</h3>
-                        <p>App that allows the user to play a virtual drum machine using different sounds and voices.</p>
+                        <h3>Quote Machine</h3>
+                        <p>App that deliveres quotes that can be shared in social media.</p>
                         <ul className="technologies">
                             <li>React</li>
                             <li>Javascript</li>
-                            <li>CSS Flexbox</li>
-                            <li>CSS Grid Layout</li>
+                            <li>CSS3</li>
                             <li>HTML5</li>
                         </ul>
                         <ul className="appRepoButtons">
                             <li><a href="https://codepen.io/fjplaurr/full/GVQbQe" target="_blank" className="appAnchor">See app</a></li>
-                            <li><a href="https://github.com/fjplaurr/drumMachine" className="repoAnchor">See repository</a></li>
+                            <li><a href="https://github.com/fjplaurr/quoteMachine" className="repoAnchor">See repository</a></li>
                         </ul>
                     </div>
                 </article>
                 <article>
-                    <img className="drumMachineImg"></img>
+                    <div className="image drumMachineImg"></div >
                     <div className="projectInfo">
                         <h3>Drum Machine</h3>
                         <p>App that allows the user to play a virtual drum machine using different sounds and voices.</p>
