19a72efc02d478beccbae988b470b538408ca894
Francisco Javier <fjplaurr@gmail.com>
2019-12-29 22:12:47 +0100

    Changes in intro page design

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -22,6 +22,7 @@ export default function intro() {
         {/* eslint-disable-next-line max-len */}
         <p>I make useful products that provide a sweet user experience following three principles: simplicity, responsive design and attention to detail.</p> 
       </div>
+      <img src={personalLogo} className="personalLogo" alt="Personal logo from Francisco Plaza" />
     </div>
   );
 }
