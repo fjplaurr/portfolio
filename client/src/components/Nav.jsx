49a52c91e57e121aeaa2cc7b98419ea69b91e19b
Francisco Javier <fjplaurr@gmail.com>
2019-12-29 22:03:15 +0100

    Deleted unusued dependencies

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import personalLogo from '../../images/personalLogo.svg';
-
 
 export default function Nav() {
   return (
@@ -11,8 +9,6 @@ export default function Nav() {
         <a href="#skills" className="anchorNav">Skills</a>
         <a href="#gridWrapper" className="anchorNav">Portfolio</a>
         <a href="#footer" className="anchorNav">Contact</a>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a className="anchorNav logoNav"><img src={personalLogo} alt="Personal logo from Francisco Plaza" /></a>
       </nav>
     </div>
   );
